a8ade357db20650252da78ef6befe397f6830207
KeLi <keliwork@outlook.com>
2019-12-19 17:30:55 +0800

    Adds Email moudle.

---
+++ KeLi.Common.Tool/Email/EmailHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace KeLi.Common.Tool.Email
+{
+    /// <summary>
+    /// Email helper.
+    /// </summary>
+    public static class EmailHelper
+    {
+        /// <summary>
+        /// Sends mail message.
+        /// </summary>
+        /// <param name="former"></param>
+        /// <param name="mailAddress"></param>
+        /// <param name="mailSubject"></param>
+        /// <param name="mailBody"></param>
+        /// <param name="isHtml"></param>
+        public static void SendMail(this FromerInfo former, string mailAddress, string mailSubject, string mailBody, bool isHtml = false)
+        {
+            try
+            {
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(former.FromAddress, former.DisplayName, Encoding.UTF8),
+                    Subject = mailSubject,
+                    SubjectEncoding = Encoding.UTF8,
+                    Body = mailBody,
+                    BodyEncoding = Encoding.UTF8,
+                    IsBodyHtml = isHtml,
+                    Priority = MailPriority.Normal
+                };
+
+                if (mailAddress.IndexOf(',') > -1)
+                {
+                    var mailAddresses = mailAddress.Split(',');
+
+                    foreach (var item in mailAddresses)
+                    {
+                        if (item.Trim() != string.Empty)
+                            mail.To.Add(item);
+                    }
+                }
+
+                else
+                    mail.To.Add(mailAddress);
+
+                var client = new SmtpClient(former.Host, former.Port)
+                {
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(former.FromAddress, former.Password)
+                };
+
+                client.Send(mail);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ KeLi.Common.Tool/Email/FromerInfo.cs
@@ -0,0 +1,50 @@
+﻿namespace KeLi.Common.Tool.Email
+{
+    /// <summary>
+    /// Former info.
+    /// </summary>
+    public class FromerInfo
+    {
+        /// <summary>
+        /// News a former info instance.
+        /// </summary>
+        /// <param name="fromAddress"></param>
+        /// <param name="displayName"></param>
+        /// <param name="password"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        public FromerInfo(string fromAddress, string displayName, string password, string host, int port)
+        {
+            FromAddress = fromAddress;
+            DisplayName = displayName;
+            Password = password;
+            Host = host;
+            Port = port;
+        }
+
+        /// <summary>
+        /// Former mail address.
+        /// </summary>
+        public string FromAddress { get; set; }
+
+        /// <summary>
+        /// Former display name.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Former password.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Former host.
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Former port.
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
