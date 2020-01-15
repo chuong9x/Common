b3367a8e36b303d82a295b687ba8149e1c31ee33
KeLi <keliwork@outlook.com>
2020-01-15 10:21:33 +0800

    Adds DirectShape builder function.

---
+++ KeLi.Common.Revit/Builders/DirectShapeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using KeLi.Common.Revit.Entities;
+using static Autodesk.Revit.DB.GeometryCreationUtilities;
+
+namespace KeLi.Common.Revit.Builders
+{
+    /// <summary>
+    /// DirectShape builder.
+    /// </summary>
+    public class DirectShapeBuilder
+    {
+        /// <summary>
+        /// Creates DirectShape.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="parm"></param>
+        /// <param name="opt"></param>
+        /// <returns></returns>
+        public static DirectShape CreateDirectShape(Document doc, ExtrusionParm parm, SolidOptions opt = null)
+        {
+            var solid = CreateExtrusionGeometry(parm.Profile, parm.Direction, parm.Distance);
+
+            if (opt != null)
+                solid = CreateExtrusionGeometry(parm.Profile, parm.Direction, parm.Distance, opt);
+
+            var result = DirectShape.CreateElement(doc, new ElementId(parm.Category));
+
+            result?.AppendShape(new List<GeometryObject> { solid });
+
+            return result;
+        }
+    }
+}
---
+++ KeLi.Common.Revit/Entities/ExtrusionParm.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace KeLi.Common.Revit.Entities
+{
+    /// <summary>
+    /// Extrusion paramter.
+    /// </summary>
+    public class ExtrusionParm
+    {
+        /// <summary>
+        /// Extrusion paramter.
+        /// </summary>
+        /// <param name="profile"></param>
+        /// <param name="direction"></param>
+        /// <param name="distance"></param>
+        /// <param name="category"></param>
+        public ExtrusionParm(List<CurveLoop> profile, XYZ direction, double distance, BuiltInCategory category)
+        {
+            Profile = profile;
+            Direction = direction;
+            Distance = distance;
+            Category = category;
+        }
+
+        /// <summary>
+        /// Extrusion's profile.
+        /// </summary>
+        public List<CurveLoop> Profile { get; set; }
+
+        /// <summary>
+        /// Extrusion's direction.
+        /// </summary>
+        public XYZ  Direction { get; set; }
+
+        /// <summary>
+        /// Extrusion's distance.
+        /// </summary>
+        public double  Distance { get; set; }
+
+        /// <summary>
+        /// Extrusion's category.
+        /// </summary>
+        public BuiltInCategory Category { get; set; }
+    }
+}
